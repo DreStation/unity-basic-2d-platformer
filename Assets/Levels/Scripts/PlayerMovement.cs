dc72b9adacc0e6e60ad7647a32e45889292d1665
asouvanlasy <asouvanlasy@outlook.com>
2022-05-21 18:59:14 -0400

    Movement, wall jumping, shooting

---
+++ Assets/Levels/Scripts/PlayerMovement.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    private Animator anim;
+    private Rigidbody2D body;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
+
+    private void Awake()
+    {
+        // Check if these components exist
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Flip player when moving left or right
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // Set animation parameters
+        anim.SetBool("run", horizontalInput != 0); // Use run animation when player is moving horizontally
+        anim.SetBool("isGrounded", isGrounded()); // Returns true when player is touching a "Ground" tag
+
+        if (wallJumpCooldown > 0.2f)
+        {
+            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                // Player doesn't fall off wall
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 7;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded()) // Normal jump
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded()) // Wall jump
+        {
+            if (horizontalInput == 0) // For getting off the wall, face the player in the direction of movement
+            {
+                // Mathf.Sign returns 1 when facing right, and -1 when facing left.
+                // The negative is for pushing away from the wall
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else // For climbing the wall, keep the player facing the wall
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+            wallJumpCooldown = 0;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        // Create an invisible box under the player to detect the ground
+        // The parameters are for size and direction of the box
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded() && !onWall();
+    }
+}
