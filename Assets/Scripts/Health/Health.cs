7d70eeca91aa76c18c43e36c2ec52205728de6e1
asouvanlasy <asouvanlasy@outlook.com>
2022-06-06 13:51:13 -0400

    Added HP + HP UI, traps, HP pickup

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,6 +4,7 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; } // Var can be grabbed from outside, but can only be set inside this script
+    private bool dead;
     
     private Animator anim;
 
@@ -13,18 +14,30 @@ public class Health : MonoBehaviour
         anim = GetComponent<Animator>(); // Get Animator to play hurt and death animation
     }
 
-    private void TakeDamage(float _damage)
+    public void TakeDamage(float _damage)
     {
         // Health can't go below 0 or above max health
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0) // Player hurt
         {
-            anim.SetTrigger("hurt");
+            anim.SetTrigger("hurt"); // Play hurt animation
         }
         else // Player dead
         {
-            anim.SetTrigger("die");
+            if (!dead)
+            {
+                anim.SetTrigger("die"); // Play death animation
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+
         }
+    }
+
+    public void AddHealth(float _value)
+    {
+        // Health can't go below 0 or above max health
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
 }
